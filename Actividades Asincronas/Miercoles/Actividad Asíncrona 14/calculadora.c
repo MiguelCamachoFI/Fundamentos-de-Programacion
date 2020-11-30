62ce03b51962013256a016ac1da3e4e64fc9b098
Miguel Angel Camacho Garduño <41399255+Mike-Camacho@users.noreply.github.com>
2020-11-30 14:24:52 -0600

    Update calculadora.c

--- Actividades Asincronas/Miercoles/Actividad Asíncrona 14/calculadora.c
+++ Actividades Asincronas/Miercoles/Actividad Asíncrona 14/calculadora.c
@@ -1,26 +1,36 @@
 #include<stdio.h>
-int main()
+float main()
 {
     printf("bienvenido\n\n");
     //Declarar variables
-    int a,b,s,r,mu,d,mo;
+    float a,b,s,r,mu,d;
+    int mo,x,y;
     //Solicitar variables
     printf("Dame 2 números seprados por espacios:");
-    scanf("%i %i",&a,&b);
+    scanf("%f %f",&a,&b);
     //Suma
     s=a+b;
-    printf("La suma de %d y %d es: %d \n",a,b,s);
+    printf("La suma de %f y %f es: %f \n",a,b,s);
     //Resta
     r=a-b;
-    printf("La resta de %d y %d es: %d \n",a,b,r);
+    printf("La resta de %f y %f es: %F \n",a,b,r);
     //Multiplicación
     mu=a*b;
-    printf("La multiplicación de %d y %d es: %d \n",a,b,mu);
+    printf("La multiplicación de %f y %f es: %f \n",a,b,mu);
     //División
     d=a/b;
-    printf("La división de %d y %d es: %d \n",a,b,d);
+    if (b==0)
+    {
+        printf("la división es indeterminada\n");
+    }
+    else
+    {
+        printf("La división de %f y %f es: %f \n",a,b,d);
+    }
     //modulo o residuo
-    mo=a%b;
-    printf("El módulo o residuo de %d y %d es: %d \n",a,b,mo);
+    x=a;
+    y=b;
+    mo = x % y;
+    printf("El módulo o residuo de %d y %d es: %d \n", x, y, mo);
     return 0;
 }
