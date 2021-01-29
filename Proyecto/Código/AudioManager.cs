69a4041622a2b2b7dfd227762e7107a64d9f5876
Miguel Angel Camacho Garduño <41399255+Mike-Camacho@users.noreply.github.com>
2021-01-29 17:53:11 -0600

    Add files via upload

---
+++ Proyecto/Código/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer[] audioMixers;
+    // Se va a evaluar la duración del audio
+    public AudioMixerGroup[] FindMatchingGroups(string subPath)
+    {
+        for (int i = 0; i < audioMixers.Length; i++)
+        {
+            AudioMixerGroup[] results = audioMixers[i].FindMatchingGroups(subPath);
+            if (results != null && results.Length != 0)
+            {
+                return results;
+            }
+        }
+
+        return null;
+    }
+    // Se va a evaluar la duración del audio
+    public void SetFloat(string name, float value)
+    {
+        for (int i = 0; i < audioMixers.Length; i++)
+        {
+            if (audioMixers[i] != null)
+            {
+                audioMixers[i].SetFloat(name, value);
+            }
+        }
+    }
+    // Se va a evaluar la duración del audio
+    public void GetFloat(string name, out float value)
+    {
+        value = 0f;
+        for (int i = 0; i < audioMixers.Length; i++)
+        {
+            if (audioMixers[i] != null)
+            {
+                audioMixers[i].GetFloat(name, out value);
+                break;
+            }
+        }
+    }
+}
---
+++ Proyecto/Código/ChargedProjectileEffectsHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargedProjectileEffectsHandler : MonoBehaviour
+{
+    [Tooltip("Object that will be affected by charging scale & color changes")]
+    public GameObject chargingObject;
+    [Tooltip("Scale of the charged object based on charge")]
+    public MinMaxVector3 scale;
+    [Tooltip("Color of the charged object based on charge")]
+    public MinMaxColor color;
+
+    MeshRenderer[] m_AffectedRenderers;
+    ProjectileBase m_ProjectileBase;
+
+    private void OnEnable()
+    {
+        m_ProjectileBase = GetComponent<ProjectileBase>();
+        DebugUtility.HandleErrorIfNullGetComponent<ProjectileBase, ChargedProjectileEffectsHandler>(m_ProjectileBase, this, gameObject);
+
+        m_ProjectileBase.onShoot += OnShoot;
+
+        m_AffectedRenderers = chargingObject.GetComponentsInChildren<MeshRenderer>();
+        // Cada vez que se lanza un corete, se altera los renders
+        foreach (var ren in m_AffectedRenderers)
+        {
+            ren.sharedMaterial = Instantiate(ren.sharedMaterial);
+        }
+    }
+
+    void OnShoot()
+    {
+        chargingObject.transform.localScale = scale.GetValueFromRatio(m_ProjectileBase.initialCharge);
+        // Cada vez que se lanza un corete, va a cambiar de color el arma
+        foreach (var ren in m_AffectedRenderers)
+        {
+            ren.sharedMaterial.SetColor("_Color", color.GetValueFromRatio(m_ProjectileBase.initialCharge));
+        }
+    }
+}
